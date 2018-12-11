f32f6fe0f3f50001c0f1c14c417dd7a2d231ac03
Gedson Marcelino <gedson.marcelino@gmail.com>
2018-12-11 11:20:10 -0300

    - refactored 2-olx

--- 2-olx/index.js
+++ 2-olx/index.js
@@ -4,27 +4,21 @@ const cheerio = require('cheerio');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   await page.goto('https://al.olx.com.br/alagoas/maceio/computadores-e-acessorios?pe=3000&q=macbook');
   
   const html = await page.content();
   const $ = cheerio.load(html);
-
   const items = $('ul#main-ad-list>li.item');
   
   let total = 0;
-  for ( let i = 0; i<items.length; i++ ){
-    const item = items.get(i);    
-    
+  items.each( (index, item) => {   
     const title = $(item).find('h2.OLXad-list-title').text().trim();
-    if ( title === '' ) continue;
-
-    const value = $(item).find('div.col-3').text().replace(/\s/g, "");
-    total++;
-
-    console.log('-->', title, value, $(item).hasClass('yap-loaded') );    
-  }
-
+    if ( title !== '' ) {
+      const value = $(item).find('div.col-3').text().replace(/\s/g, "");
+      console.log('-->', title, value, $(item).hasClass('yap-loaded') );    
+      total++;      
+    }    
+  });
   console.log('Size: ', total);
 
   await browser.close();
